71ca8cafddddc3d84224d838b4997ff91ba94933
李昕亮 <lixinliang@ones.cn>
2024-04-30 14:38:04 +0800

    feat: update control

--- entry/components/control/index.tsx
+++ entry/components/control/index.tsx
@@ -1,19 +1,13 @@
-import { Focal } from '@/components/focal'
 import { Photo } from '@/components/photo'
 import { Camera } from '@/components/camera'
 import { Shutter } from '@/components/shutter'
 
 export const Control = () => {
   return (
-    <>
-      <div className="flex justify-center items-center w-full pb-6">
-        <Focal />
-      </div>
-      <div className="flex justify-between items-center w-full px-6">
-        <Photo />
-        <Shutter />
-        <Camera />
-      </div>
-    </>
+    <div className="flex justify-between items-center w-full px-6">
+      <Photo />
+      <Shutter />
+      <Camera />
+    </div>
   )
 }
